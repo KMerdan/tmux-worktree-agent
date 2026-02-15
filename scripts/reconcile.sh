b531efac2ce976bc06e2ed1beb8ab5d685c48291
Dr. Kiji <merdan.jp@gmail.com>
2026-02-15 13:46:36 +0900

    chore: remove debug logging

--- scripts/reconcile.sh
+++ scripts/reconcile.sh
@@ -18,10 +18,6 @@ fi
 
 # Main reconciliation
 main() {
-    # Debug: log to file
-    exec 2>> /tmp/reconcile-debug.log
-    set -x
-
     log_info "Reconciling worktree sessions..."
     echo ""
 
